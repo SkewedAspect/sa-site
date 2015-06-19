3be604d858e040c142cf59cd31131c901f47bd0b
Travis Odom <todom@armt.com>
2015-06-19 14:00:51 -0500

    PoC Slackin-Angular integration added

--- js/app.js
+++ js/app.js
@@ -5,27 +5,32 @@
 // ---------------------------------------------------------------------------------------------------------------------
 
 angular.module('sa', [
-        'ngRoute',
-        'sa.controllers'
-    ])
-    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider)
-    {
-        $locationProvider.html5Mode(true);
+    'ngRoute',
+    'sa.controllers',
+    'ngLoadScript'
+])
+    .config([
+        '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider)
+        {
+            $locationProvider.html5Mode(true);
 
-        $routeProvider
-            .when('/', { templateUrl: 'partials/home.tpl.html', controller: 'HomeController'})
-            .when('/contact', { templateUrl: 'partials/contact.tpl.html', controller: 'ContactController' })
-            .otherwise('/');
-    }])
-    .run(['$rootScope', '$location', function($rootScope, $location)
-    {
-        Object.defineProperty($rootScope, 'path', {
-            get: function()
-            {
-                return $location.path();
-            }
-        });
-    }]);
+            $routeProvider
+                .when('/', { templateUrl: 'partials/home.tpl.html', controller: 'HomeController' })
+                .when('/contact', { templateUrl: 'partials/contact.tpl.html', controller: 'ContactController' })
+                .otherwise('/');
+        }
+    ])
+    .run([
+        '$rootScope', '$location', function($rootScope, $location)
+        {
+            Object.defineProperty($rootScope, 'path', {
+                get: function()
+                {
+                    return $location.path();
+                }
+            });
+        }
+    ]);
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Modules
